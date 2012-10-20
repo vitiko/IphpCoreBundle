49f734c9d92d15abe0849294012bcf4577ccb6c2
vitiko <vitiko@mail.ru>
2012-10-20 14:22:10 +0400

    1

--- Kernel/RegisterBundles.php
+++ Kernel/RegisterBundles.php
@@ -2,7 +2,30 @@
 /**
  * @author Vitiko <vitiko@mail.ru>
  */
+
+namespace Iphp\CoreBundle\Kernel;
+
 class RegisterBundles
 {
+
+    static function regisger(\Symfony\Component\HttpKernel\Kernel $kernel)
+    {
+        return array(
+            new \Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new \Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            new \Symfony\Bundle\TwigBundle\TwigBundle(),
+            new \Symfony\Bundle\MonologBundle\MonologBundle(),
+            new \Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+            new \Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new \Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+            new \Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new \JMS\AopBundle\JMSAopBundle(),
+            new \JMS\DiExtraBundle\JMSDiExtraBundle($kernel),
+            new \JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+
+            new \Sonata\EasyExtendsBundle\SonataEasyExtendsBundle(),
+
+        );
+    }
 
 }
