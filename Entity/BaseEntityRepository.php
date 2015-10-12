8b40171da44f5c5eda634ca674b1b52330afcd4e
Ural <u.davletshin@informika.ru>
2015-10-12 11:07:40 +0300

    Updates for Doctrine ORM 2.5

--- Entity/BaseEntityRepository.php
+++ Entity/BaseEntityRepository.php
@@ -3,24 +3,22 @@
 namespace Iphp\CoreBundle\Entity;
 
 use Doctrine\ORM\EntityRepository;
-use Doctrine\ORM\QueryBuilder;
-
 
 abstract class BaseEntityRepository extends EntityRepository
 {
-
-
     protected function getDefaultQueryBuilder(\Doctrine\ORM\EntityManager $em)
     {
         return new BaseEntityQueryBuilder ($em);
     }
 
-
     /**
      * @param string $alias
+     * @param string $indexBy The index for the from.
+     * @param \Closure $prepareQueryBuilder
+     *
      * @return \Iphp\CoreBundle\Entity\BaseEntityQueryBuilder
      */
-    public function createQueryBuilder($alias = '', \Closure $prepareQueryBuilder = null)
+    public function createQueryBuilder($alias = '', $indexBy = null, \Closure $prepareQueryBuilder = null)
     {
         $qb = $this->getDefaultQueryBuilder($this->_em)
             ->setEntityName($this->_entityName)
@@ -35,32 +33,28 @@ abstract class BaseEntityRepository extends EntityRepository
         return $qb;
     }
 
-
     /**
-     * @param Closure $prepareQueryBuidler
+     * @param \Closure $prepareQueryBuidler
      * @param string $alias
      * @return \Doctrine\ORM\Query
      */
     public function createQuery($alias = '', \Closure $prepareQueryBuidler = null)
     {
-        return $this->createQueryBuilder($alias, $prepareQueryBuidler)->getQuery();
+        return $this->createQueryBuilder($alias, null, $prepareQueryBuidler)->getQuery();
     }
-
 
     public function countRows($alias = '', \Closure $prepareQueryBuidler = null, $distinct = false)
     {
-        $qb = $this->createQueryBuilder($alias, $prepareQueryBuidler);
+        $qb = $this->createQueryBuilder($alias, null, $prepareQueryBuidler);
 
  
 
         return $this->countQueryRows($qb, $distinct );
     }
 
-
     public function countQueryRows(BaseEntityQueryBuilder $qb, $distinct = false)
     {
         $res =   $qb->getCountClone($distinct)->getQuery()->getOneOrNullResult();
         return $res['rownum'];
     }
-
 }
